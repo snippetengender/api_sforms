41289964ef4769928c330fe11630cf0ff8afd51b
Mukund <testingmukund04@gmail.com>
2025-12-05 09:24:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -170,4 +170,5 @@ def get_form_responses(form_slug: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="localhost", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
